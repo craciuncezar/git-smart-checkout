c61dd9a3683fd170862b33c46c3c824740daa226
craciuncezar1996@gmail.com <cezar.craciun@cezars-mbp.lan>
2022-01-09 19:35:05 +0200

    Show cursor after interrupt

--- src/interactions.rs
+++ src/interactions.rs
@@ -1,21 +1,19 @@
+use anyhow::Result;
 use dialoguer::{theme::ColorfulTheme, Confirm, FuzzySelect};
 
 use crate::git::GitBranch;
 
-pub fn select_branch(branches: &[GitBranch]) -> &GitBranch {
+pub fn select_branch(branches: &[GitBranch]) -> Result<&GitBranch> {
     let selection_index = FuzzySelect::with_theme(&ColorfulTheme::default())
         .with_prompt("Search git branch by name")
         .default(0)
         .items(&branches[..])
-        .interact()
-        .unwrap();
-
-    &branches[selection_index]
+        .interact()?;
+    Ok(&branches[selection_index])
 }
 
-pub fn confirm_git_stash() -> bool {
-    Confirm::with_theme(&ColorfulTheme::default())
+pub fn confirm_git_stash() -> Result<bool> {
+    Ok(Confirm::with_theme(&ColorfulTheme::default())
         .with_prompt("Stash changes before switch branches?")
-        .interact()
-        .unwrap()
+        .interact()?)
 }
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,13 @@ mod git;
 mod interactions;
 
 fn main() {
+    ctrlc::set_handler(show_cursor).unwrap();
+
     let branches = git::utils::hoist_selected_branch(git::client::get_git_branches());
-    let selected_branch = interactions::select_branch(&branches);
+    let selected_branch = interactions::select_branch(&branches).unwrap_or_else(|_| {
+        show_cursor();
+        std::process::exit(0);
+    });
 
     if selected_branch.selected {
         println!("You are already on branch {}", selected_branch.name);
@@ -15,10 +20,20 @@ fn main() {
     if !switch_branch.stderr.is_empty() {
         let error = String::from_utf8_lossy(&switch_branch.stderr);
         if error.contains("Please commit your changes or stash them before you switch branches.") {
-            if interactions::confirm_git_stash() {
+            let should_stash = interactions::confirm_git_stash().unwrap_or_else(|_| {
+                show_cursor();
+                std::process::exit(0);
+            });
+            if should_stash {
                 git::client::stash_git_changes();
                 git::client::switch_to_branch(&selected_branch);
             }
         };
     }
+}
+
+// make sure that the cursor re-appears when interrupting
+fn show_cursor() {
+    let term = dialoguer::console::Term::stderr();
+    let _ = term.show_cursor();
 }
