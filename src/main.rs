c374567caeb89209a2a9d7b962ea0bea350f6c60
craciuncezar1996@gmail.com <cezar.craciun@cezars-mbp.lan>
2022-01-09 15:36:30 +0200

    Remove unnecesarry struct

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use git_client::{utils, GitClient};
+use git_client::utils;
 mod git_client;
 mod interactions;
 
 fn main() {
-    let branches = utils::hoist_selected_branch(GitClient::get_git_branches());
+    let branches = utils::hoist_selected_branch(git_client::get_git_branches());
     let selected_branch = interactions::select_branch(&branches);
 
     if selected_branch.selected {
@@ -11,14 +11,14 @@ fn main() {
         return;
     }
 
-    let switch_branch = GitClient::switch_to_branch(&selected_branch);
+    let switch_branch = git_client::switch_to_branch(&selected_branch);
 
     if !switch_branch.stderr.is_empty() {
         let error = String::from_utf8_lossy(&switch_branch.stderr);
         if error.contains("Please commit your changes or stash them before you switch branches.") {
             if interactions::confirm_git_stash() {
-                GitClient::stash_git_changes();
-                GitClient::switch_to_branch(&selected_branch);
+                git_client::stash_git_changes();
+                git_client::switch_to_branch(&selected_branch);
             }
         };
     }
